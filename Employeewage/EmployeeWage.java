c5fb04e93f10cf1716fba7eb8fb681860ad6b7e1
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-04 00:37:34 +0530

    Added   Employeewage/EmployeeWage.java Program

--- Employeewage/EmployeeWage.java
+++ Employeewage/EmployeeWage.java
@@ -29,6 +29,9 @@ public class EmployeeWage {
     //Making private to use abstraction concept
     private void Employeecheck(){
         int EmployeeAttendance= 0;
+        int Dailywage=20;
+        int EmployeeHour=0;
+        int DailyEmployeeWage=0;
         //Using random class
         Random rand= new Random();
         EmployeeAttendance= rand.nextInt(2);
@@ -36,9 +39,14 @@ public class EmployeeWage {
 
         if(EmployeeAttendance == 1){
             System.out.println("Employee is present!");
+            EmployeeHour=8;
         }
         else {
             System.out.println("Employee is absent!");
+            EmployeeHour=0;
+
         }
+        DailyEmployeeWage=Dailywage*EmployeeHour;
+        System.out.println("Daily Employee Wage:"+DailyEmployeeWage);
     }
 }
