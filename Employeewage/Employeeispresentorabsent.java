5a858d7286888fc021e3999c8e1aa71be43a0828
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-03 17:53:56 +0530

    Added   Employeewage/ Program

---
+++ Employeewage/Employeeispresentorabsent.java
@@ -0,0 +1,7 @@
+package Employeewage;
+
+public class Employeeispresentorabsent {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
